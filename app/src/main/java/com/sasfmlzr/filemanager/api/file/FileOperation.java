0d2b69d8af38c208bbeb89f0ba2b461408e558a5
sasfmlzr <sasfmlzr@yandex.ru>
2018-04-10 23:16:39 +0300

    prepare to read path files

--- app/src/main/java/com/sasfmlzr/filemanager/api/file/FileOperation.java
+++ app/src/main/java/com/sasfmlzr/filemanager/api/file/FileOperation.java
@@ -1,4 +1,30 @@
 package com.sasfmlzr.filemanager.api.file;
 
+import android.content.Context;
+
+import com.sasfmlzr.filemanager.api.FileList;
+import com.sasfmlzr.filemanager.api.model.FileModel;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 public class FileOperation {
+
+    public List<FileModel> readPathFiles(String path, Context context){
+        List<FileModel> fileModelList = new ArrayList<>();
+        final File file = new File(path);
+        if (!fileModelList.isEmpty()){
+            fileModelList.clear();
+        }
+
+        if (file.exists() && file.canRead()) {
+            String[] list = file.list();
+        }
+
+
+
+
+        return fileModelList;
+    }
 }
