30e8fdbc7e7ca5feaf3cf6b01fcfb46fd02b47f8
sasfmlzr <sasfmlzr@yandex.ru>
2018-04-11 01:01:09 +0300

    add list files for read directory

--- app/src/main/java/com/sasfmlzr/filemanager/api/file/FileOperation.java
+++ app/src/main/java/com/sasfmlzr/filemanager/api/file/FileOperation.java
@@ -1,9 +1,11 @@
 package com.sasfmlzr.filemanager.api.file;
 
 import android.content.Context;
+import android.widget.Toast;
 
-import com.sasfmlzr.filemanager.api.FileList;
-import com.sasfmlzr.filemanager.api.model.FileModel;
+import com.sasfmlzr.filemanager.R;
+import com.sasfmlzr.filemanager.api.other.RootCommands;
+import com.sasfmlzr.filemanager.api.other.Settings;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -11,20 +13,31 @@ import java.util.List;
 
 public class FileOperation {
 
-    public List<FileModel> readPathFiles(String path, Context context){
-        List<FileModel> fileModelList = new ArrayList<>();
+    public List<String> listFiles(String path, Context context){
+        ArrayList<String> listFiles = new ArrayList<>();
+        final boolean showhidden = Settings.showHiddenFiles();
         final File file = new File(path);
-        if (!fileModelList.isEmpty()){
-            fileModelList.clear();
-        }
 
+        if (!listFiles.isEmpty())
+            listFiles.clear();
+        //if (file.exists()) {
         if (file.exists() && file.canRead()) {
             String[] list = file.list();
+
+            // add files/folder to ArrayList depending on hidden status
+            for (String aList : list) {
+                if (!showhidden) {
+                    if (aList.charAt(0) != '.')
+                        listFiles.add(path + "/" + aList);
+                } else {
+                    listFiles.add(path + "/" + aList);
+                }
+            }
+        } else if (Settings.rootAccess()) {
+            listFiles = RootCommands.listFiles(file.getAbsolutePath(), showhidden);
+        } else {
+            Toast.makeText(context, context.getString(R.string.cantreadfolder), Toast.LENGTH_SHORT).show();
         }
-
-
-
-
-        return fileModelList;
+        return listFiles;
     }
 }
