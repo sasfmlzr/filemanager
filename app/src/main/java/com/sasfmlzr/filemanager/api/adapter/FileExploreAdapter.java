f3e8f62160801f4ed85a2cc7637e349e7eb5865c
sasfmlzr <sasfmlzr@yandex.ru>
2018-04-10 16:30:30 +0300

    refactor code

--- app/src/main/java/com/sasfmlzr/filemanager/api/adapter/FileExploreAdapter.java
+++ app/src/main/java/com/sasfmlzr/filemanager/api/adapter/FileExploreAdapter.java
@@ -2,66 +2,61 @@ package com.sasfmlzr.filemanager.api.adapter;
 
 import android.content.Context;
 import android.content.res.Resources;
+import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import com.sasfmlzr.filemanager.R;
+import com.sasfmlzr.filemanager.api.model.FileModel;
 
 import java.util.List;
 
-public class FileExploreAdapter extends BaseAdapter {
-
+@SuppressWarnings("unused")
+public class FileExploreAdapter extends ArrayAdapter<FileModel> {
+    //public class FileExploreAdapter extends BaseAdapter<> {
     private final LayoutInflater mInflater;
     private final Context mContext;
     private final Resources mResources;
-
-    public FileExploreAdapter(Context context, LayoutInflater inflater){
-        mInflater=inflater;
+    private int res;
+    private List<FileModel> mFileModels;
+    public FileExploreAdapter(Context context, int resource, List<FileModel> fileModels){
+        super(context,resource,fileModels);
+        res=resource;
         mContext = context;
         mResources = context.getResources();
-    }
-
-    private List<String> mData;
-
-
-    @Override
-    public int getCount() {
-        return 0;
+        mFileModels = fileModels;
+        mInflater = LayoutInflater.from(context);
     }
 
     @Override
-    public Object getItem(int position) {
-        return null;
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return 0;
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-       // final ViewHolder viewHolder;
-        if(convertView==null){
-            convertView = mInflater.inflate(R.layout.activity_main, parent,false);
-        } else{
-
+    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+        final ViewHolder mViewHolder;
+        //final File file = new File(getItem(position));
+        if(convertView==null)
+        {
+            convertView = mInflater.inflate(R.layout.current_item_file, parent,false);
+            mViewHolder = new ViewHolder(convertView);
+            convertView.setTag(mViewHolder);
+        } else
+        {
+            mViewHolder = (ViewHolder) convertView.getTag();
         }
-
+        FileModel fileModel= mFileModels.get(position);
+        mViewHolder.dateView.setText(fileModel.getDateFile());
+        mViewHolder.bottomView.setText(fileModel.getNameFile());
+        mViewHolder.nameView.setText(fileModel.getTypeFile());
         return convertView;
     }
 
     private class ViewHolder {
-
         final TextView nameView, bottomView,dateView;
         ViewHolder(View view){
-            nameView = (TextView) view.findViewById(R.id.file_name_view);
-            bottomView = (TextView) view.findViewById(R.id.bottom_view);
-            dateView = (TextView) view.findViewById(R.id.date_view);
-         //   capitalView = (TextView) view.findViewById(R.id.capital);
+            nameView =      view.findViewById(R.id.file_name_view);
+            bottomView =    view.findViewById(R.id.bottom_view);
+            dateView =      view.findViewById(R.id.date_view);
         }
     }
 
