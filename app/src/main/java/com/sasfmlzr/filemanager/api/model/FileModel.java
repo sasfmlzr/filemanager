7c74b4a97e5f04a1da089f7c62a74bdc47d23dcb
sasfmlzr <sasfmlzr@yandex.ru>
2018-04-10 16:30:56 +0300

    add getters and setters

--- app/src/main/java/com/sasfmlzr/filemanager/api/model/FileModel.java
+++ app/src/main/java/com/sasfmlzr/filemanager/api/model/FileModel.java
@@ -6,5 +6,32 @@ public class FileModel {
     private String typeFile;
     private String dateFile;
 
+    public FileModel(String nameFile, String typeFile, String dateFile){
+        this.nameFile = nameFile;
+        this.typeFile = typeFile;
+        this.dateFile = dateFile;
+    }
+    public String getNameFile() {
+        return nameFile;
+    }
 
+    public void setNameFile(String nameFile) {
+        this.nameFile = nameFile;
+    }
+
+    public String getTypeFile() {
+        return typeFile;
+    }
+
+    public void setTypeFile(String typeFile) {
+        this.typeFile = typeFile;
+    }
+
+    public String getDateFile() {
+        return dateFile;
+    }
+
+    public void setDateFile(String dateFile) {
+        this.dateFile = dateFile;
+    }
 }
