2bcd3c426f84b8f43f6a2bb8de1b5158875c7f14
sasfmlzr <sasfmlzr@yandex.ru>
2018-04-10 16:32:08 +0300

    add example fill List

--- app/src/main/java/com/sasfmlzr/filemanager/MainActivity.java
+++ app/src/main/java/com/sasfmlzr/filemanager/MainActivity.java
@@ -2,12 +2,50 @@ package com.sasfmlzr.filemanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+import com.sasfmlzr.filemanager.api.adapter.FileExploreAdapter;
+import com.sasfmlzr.filemanager.api.model.FileModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ListView mFileList;
+    private FileExploreAdapter mFileExploreAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
     }
+
+    private void init() {
+        mFileList = findViewById(R.id.mFileList);
+        List<FileModel> fileModel = new ArrayList<>();
+        fileModel = fileModelLoad();
+
+        mFileExploreAdapter = new FileExploreAdapter(this, R.layout.current_item_file, fileModel);
+        mFileList.setAdapter(mFileExploreAdapter);
+    }
+
+
+    private List<FileModel> fileModelLoad (){
+
+        List<FileModel> mfileModel = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            mfileModel.add(new FileModel("asds","asdas","sadasa"));
+        }
+        return mfileModel;
+    }
+
+
+
+    public void onClick(View view){
+        System.out.print("asd");
+    }
+
 }
