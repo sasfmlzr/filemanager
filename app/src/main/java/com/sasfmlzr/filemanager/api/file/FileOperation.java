ae469163f63094887eceec3cfcc73a247a4e83e0
sasfmlzr <sasfmlzr@yandex.ru>
2018-04-13 15:45:29 +0300

    delete show hidden files and root features

--- app/src/main/java/com/sasfmlzr/filemanager/api/file/FileOperation.java
+++ app/src/main/java/com/sasfmlzr/filemanager/api/file/FileOperation.java
@@ -6,8 +6,7 @@ import android.widget.Toast;
 import com.sasfmlzr.filemanager.R;
 import com.sasfmlzr.filemanager.api.adapter.FileExploreAdapter;
 import com.sasfmlzr.filemanager.api.model.FileModel;
-import com.sasfmlzr.filemanager.api.other.RootCommands;
-import com.sasfmlzr.filemanager.api.other.Settings;
+
 import java.io.File;
 import java.text.DateFormat;
 import java.util.ArrayList;
@@ -23,7 +22,6 @@ public class FileOperation {
 
     private List<String> listFiles(String path, Context context){
         ArrayList<String> listFiles = new ArrayList<>();
-        final boolean showHidden = Settings.showHiddenFiles();
         final File file = new File(path);
         if (!listFiles.isEmpty()) {
             listFiles.clear();
@@ -31,15 +29,8 @@ public class FileOperation {
         if (file.exists() && file.canRead()) {
             String[] list = file.list();
             for (String aList : list) {
-                if (!showHidden) {
-                    if (aList.charAt(0) != '.')
-                        listFiles.add(path + "/" + aList);
-                } else {
-                    listFiles.add(path + "/" + aList);
-                }
+                listFiles.add(path + "/" + aList);
             }
-        } else if (Settings.rootAccess()) {
-            listFiles = RootCommands.listFiles(file.getAbsolutePath(), showHidden);
         } else {
             Toast.makeText(context, context.getString(R.string.cant_read_folder), Toast.LENGTH_SHORT).show();
         }
@@ -90,7 +81,6 @@ public class FileOperation {
             }
             FileOperation fileOperation = new FileOperation();
             fileModel.addAll(fileOperation.fileModelLoad(path, context));
-            Settings.updatePreferences(context);
             mFileExploreAdapter = new FileExploreAdapter(context, R.layout.current_item_file, fileModel);
             return mFileExploreAdapter;
         } else {
