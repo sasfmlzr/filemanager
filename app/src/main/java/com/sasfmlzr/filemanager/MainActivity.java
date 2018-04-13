9d782e6a09b3806073c6d647c0de978ce7f5f52c
sasfmlzr <sasfmlzr@yandex.ru>
2018-04-13 13:55:46 +0300

    delete abstract class

--- app/src/main/java/com/sasfmlzr/filemanager/MainActivity.java
+++ app/src/main/java/com/sasfmlzr/filemanager/MainActivity.java
@@ -1,41 +1,52 @@
 package com.sasfmlzr.filemanager;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
+import com.sasfmlzr.filemanager.api.adapter.FileExploreAdapter;
+import com.sasfmlzr.filemanager.api.file.FileOperation;
 import com.sasfmlzr.filemanager.api.model.FileModel;
 import java.io.File;
-
 import static com.sasfmlzr.filemanager.api.other.Param.sCountActivity;
+
 /** View activity*/
-public class MainActivity extends AbstractActivity {
+public class MainActivity extends AppCompatActivity {
+    protected final int READ_EXTERNAL_STORAGE = 0;
     private ListView mFileList;
+    private FileExploreAdapter mFileExploreAdapter;
+    private String mCurrentPath;
+    private final FileOperation mFileOperation = new FileOperation();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mFileList = findViewById(R.id.mFileList);
-        setmFileList(mFileList);
         Intent intent = getIntent();
         if(intent.hasExtra("mCurrentPath")){
             String currentPath = intent.getStringExtra("mCurrentPath");
-            setmFileExploreAdapter(getmFileOperation().loadPath(currentPath, getApplicationContext()));
-            mFileList.setAdapter(getmFileExploreAdapter());
+            mFileExploreAdapter= mFileOperation.loadPath(currentPath, getApplicationContext());
+            mFileList.setAdapter(mFileExploreAdapter);
         }
-        beforeInit(sCountActivity);
-        //Toast.makeText(this, "Main Activity",Toast.LENGTH_SHORT).show();
+        init(sCountActivity);
         AdapterView.OnItemClickListener itemListener = new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
                 FileModel fileModels = (FileModel)parent.getItemAtPosition(position);
-                setmCurrentPath(fileModels.getPathFile());
+                mCurrentPath=fileModels.getPathFile();
                 File file = new File(fileModels.getPathFile());
                 if(file.isDirectory()){
                     Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                    intent.putExtra("mCurrentPath", getmCurrentPath());
+                    intent.putExtra("mCurrentPath", mCurrentPath);
                     sCountActivity++;
                     startActivity(intent);
                 }
@@ -47,4 +58,34 @@ public class MainActivity extends AbstractActivity {
     public void onClick(View view){
     }
 
+    public void init(int countActivity){
+        if (countActivity==0){
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission is not granted", Toast.LENGTH_SHORT).show();
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, READ_EXTERNAL_STORAGE
+                );
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case READ_EXTERNAL_STORAGE: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    String path = Environment
+                            .getExternalStorageDirectory()
+                            .getAbsolutePath();
+                    mFileExploreAdapter = mFileOperation.loadPath(path, this);
+                    mFileList.setAdapter(mFileExploreAdapter);
+                } else {
+                    Toast.makeText(this, "Please allow permissions",Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
 }
