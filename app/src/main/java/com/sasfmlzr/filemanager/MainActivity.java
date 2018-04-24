3f9b079f7319b06b484932a0d080c465626ec8f2
sasfmlzr <sasfmlzr@yandex.ru>
2018-04-24 13:25:29 +0300

    refactoring code

--- app/src/main/java/com/sasfmlzr/filemanager/MainActivity.java
+++ app/src/main/java/com/sasfmlzr/filemanager/MainActivity.java
@@ -1,55 +1,61 @@
 package com.sasfmlzr.filemanager;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Environment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+
 import com.sasfmlzr.filemanager.api.fragment.FragmentFileView;
 
 public class MainActivity extends AppCompatActivity implements FragmentFileView.OnArticleSelectedListener {
-    private FragmentFileView fragment;
-
-    public void start(Context context) {
-        Intent starter = new Intent(context, MainActivity.class);
-        context.startActivity(starter);
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        if (savedInstanceState == null) {
-            fragment = FragmentFileView.newInstance(Environment
-                    .getExternalStorageDirectory()
-                    .getAbsolutePath());
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.fileviewonactivity, fragment)
-                    .commit();
-        }
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        createFragment(null);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home){
-            if (getSupportFragmentManager().getBackStackEntryCount() != 0) {
-                getSupportFragmentManager().popBackStack();
-            } else {
-                this.finish();
-            }
+        if (item.getItemId() == android.R.id.home) {
+            onClickBackHome();
         }
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public void onArticleSelected(String currentPath) {
-        fragment = FragmentFileView.newInstance(currentPath);
-        getSupportFragmentManager().beginTransaction().replace(R.id.fileviewonactivity, fragment)
-                .addToBackStack(null).commit();
+    public void onClickBackHome() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        if (fragmentManager.getBackStackEntryCount() != 0) {
+            fragmentManager.popBackStack();
+        } else {
+            this.finish();
+        }
     }
 
-    
+    public void createFragment(String currentPath) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentFileView fragment;
+        if (fragmentManager.getBackStackEntryCount() == 0) {
+            fragment = new FragmentFileView();
+            fragmentManager.beginTransaction()
+                    .add(R.id.fileviewonactivity, fragment)
+                    .addToBackStack(null)
+                    .commit();
+        } else {
+            fragment = FragmentFileView.newInstance(currentPath);
+            fragmentManager.beginTransaction()
+                    .replace(R.id.fileviewonactivity, fragment)
+                    .addToBackStack(null).commit();
+        }
+    }
+
+    @Override
+    public void onArticleSelected(String currentPath) {
+        createFragment(currentPath);
+    }
 }
