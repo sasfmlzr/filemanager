e3179af30f9f991e540cd94602a43b64a1ffe288
sasfmlzr <sasfmlzr@yandex.ru>
2018-04-27 14:12:17 +0300

    add calculate all parents method for files

--- app/src/main/java/com/sasfmlzr/filemanager/api/file/FileOperation.java
+++ app/src/main/java/com/sasfmlzr/filemanager/api/file/FileOperation.java
@@ -89,4 +89,15 @@ public class FileOperation {
             context.startActivity(intent);
         }
     }
+
+    public static List<File> getParentsFile(File file) {
+        List<File> files = new ArrayList<>();
+        files.add(file);
+        while (file.getParent() != null) {
+            file = new File(file.getParent());
+            files.add(file);
+        }
+        Collections.reverse(files);
+        return files;
+    }
 }
