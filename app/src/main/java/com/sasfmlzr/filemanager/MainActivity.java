e3ef09fc81f35d33e97e095cc355e850f1bfedcd
sasfmlzr <sasfmlzr@yandex.ru>
2018-05-10 13:02:38 +0300

    refactoring code(test)

--- app/src/main/java/com/sasfmlzr/filemanager/MainActivity.java
+++ app/src/main/java/com/sasfmlzr/filemanager/MainActivity.java
@@ -6,8 +6,6 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
@@ -16,9 +14,10 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.sasfmlzr.filemanager.api.adapter.PagerFileListAdapter;
-import com.sasfmlzr.filemanager.api.fragment.EmptyPagerFragment;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
@@ -28,42 +27,13 @@ public class MainActivity extends AppCompatActivity {
     private static final int PERMISSION_CODE_READ_EXTERNAL_STORAGE = 0;
     private ViewPager viewPager;
 
-    public void addTabs(ViewPager viewPager) {
-        PagerFileListAdapter adapter = new PagerFileListAdapter(getSupportFragmentManager(), getApplicationContext());
-        adapter.addFragment(EmptyPagerFragment.newInstance(DEFAULT_PATH));
-        adapter.addFragment(EmptyPagerFragment.newInstance(new File(DEFAULT_PATH, "Android")));
-        viewPager.setAdapter(adapter);
-    }
-
-    public void loadViewPager(Bundle savedInstanceState) {
-        viewPager = findViewById(R.id.pager);
-        if (savedInstanceState != null) {
-            PagerFileListAdapter adapter = new PagerFileListAdapter(getSupportFragmentManager(), this);
-            viewPager.setAdapter(adapter);
-        } else {
-            addTabs(viewPager);
-        }
-    }
-
-    public void requestReadPermissions() {
-        if (ContextCompat.checkSelfPermission(Objects.requireNonNull(this)
-                .getLayoutInflater()
-                .getContext(), Manifest.permission.READ_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-                    PERMISSION_CODE_READ_EXTERNAL_STORAGE);
-        }
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //onPause();
-        requestReadPermissions();
 
-        loadViewPager(savedInstanceState);
+        requestReadPermissions();
+        loadViewPager();
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayShowHomeEnabled(true);
@@ -95,22 +65,33 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onBackPressed() {
-        FragmentManager fm = getSupportFragmentManager();
-        for (Fragment frag : fm.getFragments()) {
-            if (frag.isVisible()) {
-                FragmentManager childFm = frag.getChildFragmentManager();
-                if (childFm.getBackStackEntryCount() > 0) {
-                    childFm.popBackStack();
-                    return;
-                }
-            }
-        }
-        super.onBackPressed();
-    }
-
-    @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
+    }
+
+    public void requestReadPermissions() {
+        if (ContextCompat.checkSelfPermission(Objects.requireNonNull(this)
+                .getLayoutInflater()
+                .getContext(), Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    PERMISSION_CODE_READ_EXTERNAL_STORAGE);
+        }
+    }
+
+    public void loadViewPager() {
+        viewPager = findViewById(R.id.pager);
+        addTabs(viewPager);
+    }
+
+    public void addTabs(ViewPager viewPager) {
+        List<File> files = new ArrayList<>();
+        files.add(DEFAULT_PATH);
+        files.add(new File(DEFAULT_PATH, "Android"));
+
+        PagerFileListAdapter adapter = new PagerFileListAdapter(getSupportFragmentManager(), getApplicationContext());
+        adapter.addFragment(files);
+        viewPager.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/sasfmlzr/filemanager/api/adapter/PagerFileListAdapter.java
+++ app/src/main/java/com/sasfmlzr/filemanager/api/adapter/PagerFileListAdapter.java
@@ -1,44 +1,42 @@
 package com.sasfmlzr.filemanager.api.adapter;
 
 import android.content.Context;
-import android.os.Bundle;
-import android.os.Parcelable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.util.Log;
 import android.view.ViewGroup;
 
+import com.sasfmlzr.filemanager.api.fragment.EmptyPagerFragment;
+
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 public class PagerFileListAdapter extends FragmentPagerAdapter {
 
-    private final List<Fragment> fragmentList = new ArrayList<>();
-    private FragmentManager fragmentManager;
+    private List<File> fileList = new ArrayList<>();
     private Context context;
-    private static final String KEY_FRAGMENT = "fragment";
     private static final String TAG = "PagerFileListAdapter";
 
     public PagerFileListAdapter(FragmentManager fragmentManager, Context context) {
         super(fragmentManager);
-        this.fragmentManager = fragmentManager;
         this.context = context;
     }
 
-    public void addFragment(Fragment fragment) {
-        fragmentList.add(fragment);
+    public void addFragment(List<File> files) {
+        fileList = files;
         notifyDataSetChanged();
     }
 
     @Override
     public Fragment getItem(int position) {
-        return fragmentList.get(position);
+        return EmptyPagerFragment.newInstance(fileList.get(position));
     }
 
     @Override
     public int getCount() {
-        return fragmentList.size();
+        return fileList.size();
     }
 
     @Override
@@ -47,40 +45,6 @@ public class PagerFileListAdapter extends FragmentPagerAdapter {
             return 1f;
         } else {
             return 0.5f;
-        }
-    }
-
-    @Override
-    public Parcelable saveState() {
-        Bundle state = new Bundle();
-        int i = 0;
-        for (Fragment fragment : fragmentManager.getFragments()) {
-            int itemId = i++;
-            String bundleKey = KEY_FRAGMENT + itemId;
-            if (fragment.isAdded())
-                fragmentManager.putFragment(state, bundleKey, fragment);
-        }
-        return state;
-    }
-
-    @Override
-    public void restoreState(Parcelable state, ClassLoader loader) {
-        if (state != null) {
-            Bundle bundle = (Bundle) state;
-            bundle.setClassLoader(loader);
-
-            Iterable<String> keys = bundle.keySet();
-            for (String key : keys) {
-                if (key.startsWith(KEY_FRAGMENT)) {
-                    Fragment f = fragmentManager.getFragment(bundle, key);
-                    if (f != null) {
-                        //f.setMenuVisibility(false);
-                        addFragment(f);
-                    } else {
-                        Log.w(TAG, "Bad fragment at key " + key);
-                    }
-                }
-            }
         }
     }
 
