de4645496df1620ed897fcedb36f16df429cc8fe
sasfmlzr <sasfmlzr@yandex.ru>
2018-05-10 17:53:47 +0300

    add content provider

---
+++ app/src/main/java/com/sasfmlzr/filemanager/api/other/data/AppDatabase.java
@@ -0,0 +1,36 @@
+package com.sasfmlzr.filemanager.api.other.data;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class AppDatabase extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "cacheSizeDirectory.db";
+    public static final int DATABASE_VERSION = 1;
+
+    private static AppDatabase instance = null;
+
+    public AppDatabase(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    public static AppDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = new AppDatabase(context);
+        }
+        return instance;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "CREATE TABLE " + DataCache.TABLE_NAME + "(" +
+                DataCache.Columns._ID + " INTEGER PRIMARY KEY NOT NULL, " +
+                DataCache.Columns.PATH + " TEXT NOT NULL, " +
+                DataCache.Columns.SIZE + " TEXT NOT NULL)";
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
---
+++ app/src/main/java/com/sasfmlzr/filemanager/api/provider/CacheSizeProvider.java
@@ -0,0 +1,139 @@
+package com.sasfmlzr.filemanager.api.provider;
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.sasfmlzr.filemanager.api.other.data.AppDatabase;
+import com.sasfmlzr.filemanager.api.other.data.DataCache;
+
+public class CacheSizeProvider extends ContentProvider {
+
+    private AppDatabase mOpenHelper;
+
+    /**
+     * The URI matcher.
+     */
+    private static final UriMatcher MATCHER = buildUriMatcher();
+
+
+    public static final int CACHEDIRECTORY = 100;
+    public static final int CACHEDIRECTORY_ID = 101;
+
+    private static UriMatcher buildUriMatcher() {
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        // content://com.metanit.tasktimer.provider/FRIENDS
+        matcher.addURI(DataCache.AUTHORITY, DataCache.TABLE_NAME, CACHEDIRECTORY);
+        // content://com.metanit.tasktimer.provider/FRIENDS/8
+        matcher.addURI(DataCache.AUTHORITY, DataCache.TABLE_NAME + "/#", CACHEDIRECTORY_ID);
+        return matcher;
+    }
+
+    @Override
+    public boolean onCreate() {
+        mOpenHelper = AppDatabase.getInstance(getContext());
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        final int code = MATCHER.match(uri);
+        SQLiteQueryBuilder queryBuilder = new SQLiteQueryBuilder();
+        switch (code) {
+            case CACHEDIRECTORY:
+                queryBuilder.setTables(DataCache.TABLE_NAME);
+                break;
+            case CACHEDIRECTORY_ID:
+                queryBuilder.setTables(DataCache.TABLE_NAME);
+                long taskId = DataCache.getId(uri);
+                queryBuilder.appendWhere(DataCache.Columns._ID + " = " + taskId);
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI: " + uri);
+        }
+        SQLiteDatabase db = mOpenHelper.getReadableDatabase();
+        return queryBuilder.query(db, projection, selection, selectionArgs, null, null, sortOrder);
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        final int match = MATCHER.match(uri);
+        switch (match) {
+            case CACHEDIRECTORY:
+                return DataCache.CONTENT_TYPE;
+            case CACHEDIRECTORY_ID:
+                return DataCache.CONTENT_ITEM_TYPE;
+            default:
+                throw new IllegalArgumentException("Unknown URI: " + uri);
+        }
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        final int match = MATCHER.match(uri);
+        final SQLiteDatabase db;
+        Uri returnUri;
+        long recordId;
+        switch (match) {
+            case CACHEDIRECTORY:
+                db = mOpenHelper.getWritableDatabase();
+                recordId = db.insert(DataCache.TABLE_NAME, null, values);
+                if (recordId > 0) {
+                    returnUri = DataCache.buildUri(recordId);
+                } else {
+                    throw new android.database.SQLException("Failed to insert: " + uri.toString());
+                }
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI: " + uri);
+        }
+        return returnUri;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        final int match = MATCHER.match(uri);
+        final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+
+        String selectionCriteria = selection;
+
+        if (match != CACHEDIRECTORY && match != CACHEDIRECTORY_ID)
+            throw new IllegalArgumentException("Unknown URI: " + uri);
+
+        if (match == CACHEDIRECTORY_ID) {
+            long taskId = DataCache.getId(uri);
+            selectionCriteria = DataCache.Columns._ID + " = " + taskId;
+            if ((selection != null) && (selection.length() > 0)) {
+                selectionCriteria += " AND (" + selection + ")";
+            }
+        }
+        return db.delete(DataCache.TABLE_NAME, selectionCriteria, selectionArgs);
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        final int match = MATCHER.match(uri);
+        final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        String selectionCriteria = selection;
+
+        if (match != CACHEDIRECTORY && match != CACHEDIRECTORY_ID)
+            throw new IllegalArgumentException("Unknown URI: " + uri);
+
+        if (match == CACHEDIRECTORY_ID) {
+            long taskId = DataCache.getId(uri);
+            selectionCriteria = DataCache.Columns._ID + " = " + taskId;
+            if ((selection != null) && (selection.length() > 0)) {
+                selectionCriteria += " AND (" + selection + ")";
+            }
+        }
+        return db.update(DataCache.TABLE_NAME, values, selectionCriteria, selectionArgs);
+    }
+}
